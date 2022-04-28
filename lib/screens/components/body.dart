493d0f5121c78c23758d5b7fc0fff7a82719aa89
Sen Cai <sencaichi@gmail.com>
2022-04-28 19:14:00 -0400

    added: 2 new dependencies, flutter_svg and flutter_auth, work on welcome screen

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+
+    )
+  }
+}

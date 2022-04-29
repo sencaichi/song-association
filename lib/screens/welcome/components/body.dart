69c0db77d26674bc9c23763c844589ca36497936
Sen Cai <sencaichi@gmail.com>
2022-04-29 11:58:00 -0400

    resolved: issues with dependencies, work on background/body

--- lib/screens/welcome/components/body.dart
+++ lib/screens/welcome/components/body.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:song_association/screens/welcome/components/background.dart';
 
 class Body extends StatelessWidget {
   @override

ba127723fd698be73690ec85f781e0e298c2c12c
Sen Cai <sencaichi@gmail.com>
2022-05-03 14:57:37 -0400

    txt file: list of words added

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:song_association/screens/home/components/background.dart';
+import 'package:song_association/components/rounded_botton.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+
+    )
+  }
+}

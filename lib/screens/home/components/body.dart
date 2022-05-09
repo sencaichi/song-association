acb181f3b7f59694d41ff77a69962b71938a9e1f
Sen Cai <sencaichi@gmail.com>
2022-05-09 16:34:36 -0400

    feature: countdown worked on, initialized state, added methods,  and timcard widget

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:song_association/screens/home/components/background.dart';
 import 'package:song_association/components/rounded_botton.dart';
@@ -46,15 +47,74 @@ class _RandomWordState extends State<RandomWord> {
 }
 
 class Countdown extends StatefulWidget {
+  const Countdown({Key? key}) : super(key: key);
+
   @override
   _CountdownState createState() => _CountdownState();
 }
 
 class _CountdownState extends State<Countdown> {
+  static const countdownDuration = Duration(seconds: 10);
+  Duration duration = const Duration();
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+    reset();
+  }
+
+  void reset() {
+    setState(() => duration = countdownDuration);
+  }
+
+  void subTime() {
+    const subSeconds = -1;
+
+    setState(() {
+      final seconds = duration.inSeconds + subSeconds;
+
+      duration = Duration(seconds: seconds);
+    });
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => subTime());
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(child: buildTime()),
       );
 
-  Widget buildTime() {}
+  Widget buildTime() {
+    // method to convert duration to two digits (9 -> 09);
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildTimeCard(time: seconds, header: 'SECONDS LEFT'),
+      ],
+    );
+  }
+
+  Widget buildTimeCard({required String time, required String header}) =>
+      Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text(
+            time,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 72,
+            ),
+          ));
 }
